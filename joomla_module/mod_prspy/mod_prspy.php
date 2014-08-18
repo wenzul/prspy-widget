f4b0eac96ef2d1e4ea463406481fee8426644e9e
wenzul <wenzul@users.noreply.github.com>
2014-08-18 15:51:41 +0200

    Updated joomla module, adjusted file structure between standalone and joomla module directories to merge changes easier, serverdata.php now handles adjustable realitymod.com timeout which is occasionally the case

--- joomla_module/mod_prspy/mod_prspy.php
+++ joomla_module/mod_prspy/mod_prspy.php
@@ -6,12 +6,12 @@ defined('_JEXEC') or die('Restricted access');
 $modulebase = ''.JURI::base(true).'/modules/mod_prspy/';
 $document = JFactory::getDocument();
 
-$document->addStyleSheet($modulebase.'library/prspy.css');
-$document->addStyleSheet($modulebase.'library/flags.css');
+$document->addStyleSheet($modulebase.'library/css/prspy.css');
+$document->addStyleSheet($modulebase.'library/css/flags.css');
 
 // add Java-Script
-//$document->addScript($modulebase.'library/jquery-1.11.1.min.js'); if you have not already included a version
-$document->addScript($modulebase.'library/prspy-descriptions.js');
-$document->addScript($modulebase.'library/jquery-cookie.js');
+//$document->addScript($modulebase.'library/js/jquery-1.11.1.min.js'); if you have not already included a version
+$document->addScript($modulebase.'library/js/prspy-widget.js');
+$document->addScript($modulebase.'library/js/jquery-cookie.js');
 
 require JModuleHelper::getLayoutPath('mod_prspy');
